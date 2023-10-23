ae15c296076a0ef3fe2f113476827ad33a7e69dd
ishida <OotukaAkira@PC2>
2023-10-23 14:52:26 +0900

    変更

--- store/src/main/java/com/cy/store/controller/OrderController.java
+++ store/src/main/java/com/cy/store/controller/OrderController.java
@@ -3,27 +3,34 @@ package com.cy.store.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.cy.store.entity.Order;
 import com.cy.store.service.IOrderService;
-import com.cy.store.util.JsonResult;
 
-@RestController
+@Controller
 @RequestMapping("orders")
 public class OrderController extends BaseController {
-    @Autowired
-    private IOrderService orderService;
-
-    @RequestMapping("create")
-    public JsonResult<Order> create(Integer aid, Integer[] cids, HttpSession session) {
-        // 从Session中取出uid和username
-        Integer uid = getUidFromSession(session);
-        String username = getUsernameFromSession(session);
-        // 调用业务对象执行业务
-        Order data = orderService.create(aid, cids, uid, username);
-        // 返回成功与数据
-        return new JsonResult<Order>(OK, data);
-    }
+	
+	@Autowired
+	private IOrderService orderService;
+	
+	@PostMapping("create")
+	public String create(Integer aid, Integer[] cids, HttpSession session, Model model) {
+		
+		// セッション情報取得
+		Integer uid = getUidFromSession(session);
+		String username = getUsernameFromSession(session);
+		
+		// 注文情報作成
+		Order data = orderService.create(aid, cids, uid, username);
+		
+		// 次ページにorder情報引き渡し
+		model.addAttribute("order", data);
+		
+		return "payment";
+	}
 }
--- store/src/main/java/com/cy/store/controller/PayController.java
+++ store/src/main/java/com/cy/store/controller/PayController.java
@@ -2,30 +2,27 @@ package com.cy.store.controller;
 
 import javax.servlet.http.HttpSession;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.cy.store.entity.Order;
-import com.cy.store.service.IOrderService;
-import com.cy.store.util.JsonResult;
-
-/** 处理用户相关请求的控制器类 */
-@RestController
-@RequestMapping("payment")
+@Controller
+//@RequestMapping("pay")
 public class PayController extends BaseController {
-    @Autowired
-    private IOrderService orderService;
 
-    @PostMapping("/")
-    public JsonResult<Order> create(Integer oid,HttpSession session) {
-        // 从Session中取出uid和username
-        Integer uid = getUidFromSession(session);
-        String username = getUsernameFromSession(session);
-        // 调用业务对象执行业务
-        Order data = orderService.create(oid, uid, username);
-        // 返回成功与数据
-        return new JsonResult<Order>(OK, data);
-    }
+/*	@Autowired
+	private IOrderService orderService;
+*/
+	@PostMapping("pay")
+	//引数なしで一旦調整
+	public String payment(HttpSession session, Model model) {
+		
+		// セッション情報取得
+		Integer uid = getUidFromSession(session);
+		String username = getUsernameFromSession(session);
+		
+		//何らかの処理を使う
+		
+		return "paySuccess";
+	}
 }
